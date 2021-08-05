7a9da8fdd324a006b044a4d306fb00ba0e8181f5
cptShell <repozheka@gmail.com>
2021-08-05 21:16:29 +0300

    feat: add final solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    const guessNum = this.max - Math.floor((this.max - this.min) / 2);
+    this.currentGuess = guessNum;
+    return guessNum;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.currentGuess;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.currentGuess;
+  }
 }
 
 module.exports = GuessingGame;
